f339f6df81aa90113714efe2f8d391e4aadc5b4c
Nick A Lanese <58953235+NLanese@users.noreply.github.com>
2023-09-07 11:37:35 -0400

    Update WalletAccessPlugin.swift

--- ios/Plugin/WalletAccessPlugin.swift
+++ ios/Plugin/WalletAccessPlugin.swift
@@ -18,20 +18,25 @@ public class WalletAccessPlugin: CAPPlugin {
     // Creates Reference to PassLibrary (User Wallet)
     let passLibrary = PKPassLibrary()
 
+    // This Obejctive-C Wrapped Function is still in Swift. It, however,
+    // is doing a lot of work behind the scenes to get this ready to be converted
+    // to JavaScript on the FrontEnd. This is why the only param ALWAYS has to be
+    // a CAAPLugin call, otherwise this will not work with Capactitor.
+    // However, we can still add our own custom parameters and accesses them
+    // as shown below....
     @objc func getWallet(_ call: CAPPluginCall) {
+        
+        // Had we uncommented this line below, inputValues would equal whatever value was
+        // input for the exampleParamName value
+        // let inputValues = call.getString("exampleParamName")
+        
         if PKPassLibrary.isPassLibraryAvailable() {
-            let userPasses = PKPassLibrary.passes(<#T##self: PKPassLibrary##PKPassLibrary#>)
+            let userPasses = PKPassLibrary.passes()
         }
         else{
-            return
+            // There will be no `return` in a CAAPlugin, rather we utilize the call
+            // and its unique methods
+            call.reject("No Access to Pass Library")
         }
-        
-        
-        
-        
-        let value = call.getString("value") ?? ""
-        call.resolve([
-            "value": implementation.echo(value)
-        ])
     }
 }
