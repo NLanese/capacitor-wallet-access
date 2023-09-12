9c87c8656c4d150971bd738c0a12ea41475b1bd9
Nick A Lanese <58953235+NLanese@users.noreply.github.com>
2023-09-12 12:52:56 -0400

    Swift Updates

--- ios/Plugin/WalletAccess.swift
+++ ios/Plugin/WalletAccess.swift
@@ -2,49 +2,19 @@ import Foundation
 import Capacitor
 import PassKit
 
-/**
- * Please read the Capacitor iOS Plugin Development Guide
- * here: https://capacitorjs.com/docs/plugins/ios
- */
-
-// Denotes that the WalletAccessPlugin in this swift file will be compatible with Objective-C
-@objc(WalletAccessPlugin)
-
-// Main Plugin Class : extends CAAPlugin (given by Capacitor)
-public class WalletAccessPlugin: CAPPlugin {
-    // Name of Plugin
-    private let implementation = WalletAccess()
-
-    // This Obejctive-C Wrapped Function is still in Swift. It, however,
-    // is doing a lot of work behind the scenes to get this ready to be converted
-    // to JavaScript on the FrontEnd. This is why the only param ALWAYS has to be
-    // a CAAPLugin call, otherwise this will not work with Capactitor.
-    // However, we can still add our own custom parameters and accesses them
-    // as shown below....
-    @objc func getWallet(_ call: CAPPluginCall) {
-        
-        // Had we uncommented this line below, inputValues would equal whatever value was
-        // input for the exampleParamName value
-        // let inputValues = call.getString("exampleParamName")
+public class WalletAccess: NSObject {
     
-
-        // If Pass Library is Available
-        if PKPassLibrary.isPassLibraryAvailable() {
-            // Creates Reference to PassLibrary (User Wallet)
-            let passLibrary = PKPassLibrary()
-            let userPasses = passLibrary.passes()
-            print(userPasses)
-
-            // There will be no `return` in a CAAPlugin, rather we utilize the call
-            // and its unique methods
-            call.success([
-                cards: userPasses
-            ])
-        }
-        else{
-            // There will be no `return` in a CAAPlugin, rather we utilize the call
-            // and its unique methods
-            call.reject("No Access to Pass Library")
-        }
+    public func echo(_ value: String) -> String {
+        print(value)
+        return value
+    }
+    
+    public func getWallet() -> [PKPass] {
+        let passLibrary = PKPassLibrary()
+        let userPasses = passLibrary.passes()
+        print("====== INSIDE SWIFT ======")
+        print("====== LOGGING USER PASSES NOW... ======")
+        print(userPasses)
+        return userPasses
     }
 }
--- ios/Plugin/WalletAccessPlugin.swift
+++ ios/Plugin/WalletAccessPlugin.swift
@@ -12,39 +12,53 @@ import PassKit
 
 // Main Plugin Class : extends CAAPlugin (given by Capacitor)
 public class WalletAccessPlugin: CAPPlugin {
-    // Name of Plugin
-    private let implementation = WalletAccess()
+    // Name of Plugin
+    private let implementation = WalletAccess()
 
-    // This Obejctive-C Wrapped Function is still in Swift. It, however,
-    // is doing a lot of work behind the scenes to get this ready to be converted
-    // to JavaScript on the FrontEnd. This is why the only param ALWAYS has to be
-    // a CAAPLugin call, otherwise this will not work with Capactitor.
-    // However, we can still add our own custom parameters and accesses them
-    // as shown below....
-    @objc func getWallet(_ call: CAPPluginCall) {
-        
-        // Had we uncommented this line below, inputValues would equal whatever value was
-        // input for the exampleParamName value
-        // let inputValues = call.getString("exampleParamName")
-    
+    // This Obejctive-C Wrapped Function is still in Swift. It, however,
+    // is doing a lot of work behind the scenes to get this ready to be converted
+    // to JavaScript on the FrontEnd. This is why the only param ALWAYS has to be
+    // a CAAPLugin call, otherwise this will not work with Capactitor.
+    // However, we can still add our own custom parameters and accesses them
+    // as shown below....
+    @objc func getWallet(_ call: CAPPluginCall) {
+        
+        // Had we uncommented this line below, inputValues would equal whatever value was
+        // input for the exampleParamName value
+        // let inputValues = call.getString("exampleParamName")
+    
 
-        // If Pass Library is Available
-        if PKPassLibrary.isPassLibraryAvailable() {
-            // Creates Reference to PassLibrary (User Wallet)
-            let passLibrary = PKPassLibrary()
-            let userPasses = passLibrary.passes()
-            print(userPasses)
+        // If Pass Library is Available
+        if PKPassLibrary.isPassLibraryAvailable() {
+            // Creates Reference to PassLibrary (User Wallet)
+            let passLibrary = PKPassLibrary()
+            let userPasses = passLibrary.passes()
+            print ("==== PRINTING USER PASSES ====")
+            print(userPasses)
+            
+            var passesInJSONEncodables: [[String: Any]] = []
+            
+            for pass in userPasses{
+                let passJSON : [String: Any] = [
+                    "organization": pass.organizationName,
+                    "userInfo": pass.userInfo ?? "Error Getting Info"
+//                    "passTypeIdentifier": pass.passTypeIdentifier,
+//                    "expiration": pass.auxiliaryFields[1].value,
+//                    "firstName": pass.secondaryFields[0].value,
+//                    "lastName": pass.secondaryFields[0].value,
+//                    "memberNumber": pass.primaryFields[0].value
+                ]
+                passesInJSONEncodables.append(passJSON)
+            }
 
-            // There will be no `return` in a CAAPlugin, rather we utilize the call
-            // and its unique methods
-            call.success([
-                cards: userPasses
-            ])
-        }
-        else{
-            // There will be no `return` in a CAAPlugin, rather we utilize the call
-            // and its unique methods
-            call.reject("No Access to Pass Library")
-        }
-    }
+            // There will be no `return` in a CAAPlugin, rather we utilize the call
+            // and its unique methods
+            call.resolve(["cards": passesInJSONEncodables])
+        }
+        else{
+            // There will be no `return` in a CAAPlugin, rather we utilize the call
+            // and its unique methods
+            call.reject("No Access to Pass Library")
+        }
+    }
 }
