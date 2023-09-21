f6a3dd5c58dfaaaa34f36a18d19aa81946aa721e
Nick A Lanese <58953235+NLanese@users.noreply.github.com>
2023-09-21 10:44:39 -0400

    Fixed odd isssue where some spaces were being registered as code breaking

--- ios/Plugin/WalletAccessPlugin.swift
+++ ios/Plugin/WalletAccessPlugin.swift
@@ -12,66 +12,66 @@ import PassKit
 
 // Main Plugin Class : extends CAAPlugin (given by Capacitor)
 public class WalletAccessPlugin: CAPPlugin {
-    
-    // Name of Plugin
-    private let implementation = WalletAccess()
+    // Name of Plugin
+    private let implementation = WalletAccess()
 
-    // Returns a JSON object for each Pass in the User's Wallet
-    @objc func getWallet(_ call: CAPPluginCall) {
-        // If Pass Library is Available
-        if PKPassLibrary.isPassLibraryAvailable() {
-            
-            // Creates Reference to PassLibrary (User Wallet)
-            let passLibrary = PKPassLibrary()
-            let userPasses = passLibrary.passes()
-            
-            // Creates an Array that can be converted into a JSON Object for return to JS/TS
-            var passesInJSONEncodables: [[String: Any]] = []
-            
-            // iterates through all retrieved PKPasses
-            for pass in userPasses{
-                // Fills in Basic Information
-                var passJSON : [String: Any] = [
-                    "organization": pass.organizationName,
-                    "serialNumber": pass.serialNumber,
-                ]
-                
-                // If inputs are provided, this will track other key/value pairs and return them
-                let fieldKeys = call.getArray("value") ?? [];
-                print(fieldKeys)
-                if (!fieldKeys.isEmpty){
-                    print("Cap Input Params...")
-                    print(fieldKeys)
+    // Returns a JSON object for each Pass in the User's Wallet
+    @objc func getWallet(_ call: CAPPluginCall) {
+        // If Pass Library is Available
+        if PKPassLibrary.isPassLibraryAvailable() {
+        // Creates Reference to PassLibrary (User Wallet)
+            let passLibrary = PKPassLibrary()
+            let userPasses = passLibrary.passes()
+            
+            // Creates an Array that can be converted into a JSON Object for return to JS/TS
+            var passesInJSONEncodables: [[String: Any]] = []
+            // iterates through all retrieved PKPasses
+            for pass in userPasses{
+                // Fills in Basic Information
+                var passJSON : [String: Any] = [
+                    "organization": pass.organizationName,
+                    "serialNumber": pass.serialNumber,
+                ]
+                
+                // If inputs are provided, this will track other key/value pairs and return them
+                let fieldKeys = call.getArray("value") ?? [];
+                print(fieldKeys)
+                if (!fieldKeys.isEmpty){
+                    print("Cap Input Params...")
+                    print(fieldKeys)
 
-                    // Adds content from Primary, Secondary, and Auxiliary Fields
-                    for field in fieldKeys{
-                        if let strField = field as? String{
-                            if let strKeyValue = pass.localizedValue(forFieldKey: strField) as? String{
-                                passJSON[strField] = strKeyValue
-                            }
-                        }
-                    }
-                }
-                
-                // If no inputs or they're not found since Capacitor is fucking stupid sometimes
-                else{
-                    print("No input params found")
-                }
-                
-                // Adds the Individual Pass Json Object to the Main Return Array
-                passesInJSONEncodables.append(passJSON)
-            }
+                    // Adds content from Primary, Secondary, and Auxiliary Fields
+                    for field in fieldKeys{
+                        if let strField = field as? String{
+                            if let strKeyValue = pass.localizedValue(forFieldKey: strField) as? String{
+                                passJSON[strField] = strKeyValue
+                            }
+                        }
+                    }
+                }
+                
+                // If no inputs or they're not found since Capacitor is fucking stupid sometimes
+                else{
+                    print("No input params found")
+                }
+                
+                // Adds the Individual Pass Json Object to the Main Return Array
+                passesInJSONEncodables.append(passJSON)
+            }
 
-            // There will be no `return` in a CAAPlugin, rather we utilize the call
-            // and its unique methods
-            print("Returning Passes as JSON")
-            call.resolve(["cards": passesInJSONEncodables])
-        }
-        else{
-            // There will be no `return` in a CAAPlugin, rather we utilize the call
-            // and its unique methods
-            print("No Access to Pass Library")
-            call.reject("No Access to Pass Library")
-        }
-    }
+            
+            // There will be no `return` in a CAAPlugin, rather we utilize the call
+            // and its unique methods
+            print("Returning Passes as JSON")
+            call.resolve(["cards": passesInJSONEncodables])
+        }
+        
+        // If PKPassLibrary is Unavailable
+        else{
+            // There will be no `return` in a CAAPlugin, rather we utilize the call
+            // and its unique methods
+            print("No Access to Pass Library")
+            call.reject("No Access to Pass Library")
+        }
+    }
 }
